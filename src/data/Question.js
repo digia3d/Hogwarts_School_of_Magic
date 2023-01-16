4e7b660174cbb269d67cbc8034c018db84af5f63
Aleksandra Ujvari <ujvari65@gmail.com>
2023-01-16 13:42:58 +0100

    Update components, data and assets

--- src/data/Question.js
+++ src/data/Question.js
@@ -3,16 +3,16 @@ export const questions = [
     questionText: 'What class interests you the most?',
     answerOptions: [
       {
-        answerText: 'Charms', gryffindor: 2, slytherin: 1, ravenclaw: 2, hufflepuff: 1,
+        optionText: 'Charms', gryffindor: 2, slytherin: 1, ravenclaw: 2, hufflepuff: 1,
       },
       {
-        answerText: 'Potions', gryffindor: 1, slytherin: 1, ravenclaw: 3, hufflepuff: 1,
+        optionText: 'Potions', gryffindor: 1, slytherin: 1, ravenclaw: 3, hufflepuff: 1,
       },
       {
-        answerText: 'Transfiguration', gryffindor: 1, slytherin: 2, ravenclaw: 1, hufflepuff: 3,
+        optionText: 'Transfiguration', gryffindor: 1, slytherin: 2, ravenclaw: 1, hufflepuff: 3,
       },
       {
-        answerText: 'Defense Against the Dark Arts', gryffindor: 3, slytherin: 3, ravenclaw: 1, hufflepuff: 0,
+        optionText: 'Defense Against the Dark Arts', gryffindor: 3, slytherin: 3, ravenclaw: 1, hufflepuff: 0,
       },
     ],
   },
@@ -20,16 +20,16 @@ export const questions = [
     questionText: 'What would you do if you saw a classmate being bullied?',
     answerOptions: [
       {
-        answerText: 'Tell a teacher', gryffindor: 1, slytherin: 3, ravenclaw: 1, hufflepuff: 1,
+        optionText: 'Tell a teacher', gryffindor: 1, slytherin: 3, ravenclaw: 1, hufflepuff: 1,
       },
       {
-        answerText: 'Tell the bully to stop', gryffindor: 1, slytherin: 1, ravenclaw: 1, hufflepuff: 2,
+        optionText: 'Tell the bully to stop', gryffindor: 1, slytherin: 1, ravenclaw: 1, hufflepuff: 2,
       },
       {
-        answerText: 'Tell the classmate to ignore it', gryffindor: 3, slytherin: 0, ravenclaw: 2, hufflepuff: 2,
+        optionText: 'Tell the classmate to ignore it', gryffindor: 3, slytherin: 0, ravenclaw: 2, hufflepuff: 2,
       },
       {
-        answerText: 'Join in', gryffindor: 0, slytherin: 2, ravenclaw: 1, hufflepuff: 3,
+        optionText: 'Join in', gryffindor: 0, slytherin: 2, ravenclaw: 1, hufflepuff: 3,
       },
     ],
   },
@@ -37,16 +37,16 @@ export const questions = [
     questionText: 'If you were an animal, what would you rather be?',
     answerOptions: [
       {
-        answerText: 'A lion', gryffindor: 3, slytherin: 1, ravenclaw: 2, hufflepuff: 3,
+        optionText: 'A lion', gryffindor: 3, slytherin: 1, ravenclaw: 2, hufflepuff: 3,
       },
       {
-        answerText: 'A snake', gryffindor: 2, slytherin: 4, ravenclaw: 0, hufflepuff: 1,
+        optionText: 'A snake', gryffindor: 2, slytherin: 4, ravenclaw: 0, hufflepuff: 1,
       },
       {
-        answerText: 'An eagle', gryffindor: 0, slytherin: 1, ravenclaw: 3, hufflepuff: 1,
+        optionText: 'An eagle', gryffindor: 0, slytherin: 1, ravenclaw: 3, hufflepuff: 1,
       },
       {
-        answerText: 'A badger', gryffindor: 1, slytherin: 0, ravenclaw: 1, hufflepuff: 2,
+        optionText: 'A badger', gryffindor: 1, slytherin: 0, ravenclaw: 1, hufflepuff: 2,
       },
     ],
   },
@@ -54,16 +54,16 @@ export const questions = [
     questionText: 'If you could have any superpower, what would it be?',
     answerOptions: [
       {
-        answerText: 'Invisibility', gryffindor: 1, slytherin: 1, ravenclaw: 3, hufflepuff: 1,
+        optionText: 'Invisibility', gryffindor: 1, slytherin: 1, ravenclaw: 3, hufflepuff: 1,
       },
       {
-        answerText: 'Flying', gryffindor: 1, slytherin: 1, ravenclaw: 1, hufflepuff: 3,
+        optionText: 'Flying', gryffindor: 1, slytherin: 1, ravenclaw: 1, hufflepuff: 3,
       },
       {
-        answerText: 'Super strength', gryffindor: 3, slytherin: 1, ravenclaw: 1, hufflepuff: 1,
+        optionText: 'Super strength', gryffindor: 3, slytherin: 1, ravenclaw: 1, hufflepuff: 1,
       },
       {
-        answerText: 'Mind reading', gryffindor: 1, slytherin: 3, ravenclaw: 1, hufflepuff: 1,
+        optionText: 'Mind reading', gryffindor: 1, slytherin: 3, ravenclaw: 1, hufflepuff: 1,
       },
     ],
   },
@@ -88,16 +88,16 @@ export const questions = [
     questionText: 'What would you rather do?',
     answerOptions: [
       {
-        answerText: 'Go to a party', gryffindor: 1, slytherin: 1, ravenclaw: 1, hufflepuff: 1,
+        optionText: 'Go to a party', gryffindor: 1, slytherin: 1, ravenclaw: 1, hufflepuff: 1,
       },
       {
-        answerText: 'Stay home and read', gryffindor: 1, slytherin: 1, ravenclaw: 1, hufflepuff: 1,
+        optionText: 'Stay home and read', gryffindor: 1, slytherin: 1, ravenclaw: 1, hufflepuff: 1,
       },
       {
-        answerText: 'Go on an adventure', gryffindor: 1, slytherin: 1, ravenclaw: 1, hufflepuff: 1,
+        optionText: 'Go on an adventure', gryffindor: 1, slytherin: 1, ravenclaw: 1, hufflepuff: 1,
       },
       {
-        answerText: 'Help someone', gryffindor: 1, slytherin: 1, ravenclaw: 1, hufflepuff: 1,
+        optionText: 'Help someone', gryffindor: 1, slytherin: 1, ravenclaw: 1, hufflepuff: 1,
       },
     ],
   },
