5e9aecab30fe113f1115c99cee1d8f6baf4a1675
Aleksandra Ujvari <ujvari65@gmail.com>
2026-01-19 21:32:16 +0100

    Update characters.js

--- src/redux/actions/characters.js
+++ src/redux/actions/characters.js
@@ -9,7 +9,7 @@ export const fetchCharactersByHouse = (houseName) => async (dispatch) => {
 
   try {
     const res = await axios.get(
-      `http://school-of-magic-api.onrender.com/api/v1/houses/${houseName}/characters`,
+      `https://school-of-magic-api.onrender.com/api/v1/houses/${houseName}/characters`,
     );
 
     dispatch({
